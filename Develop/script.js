21d60ebe460cb643976083b61d90dec27b083811
Matthew Kelsch <95149182+mkelsch2000@users.noreply.github.com>
2022-01-02 15:55:03 -0700

    consolidating variables & fixing password length null bug

--- Develop/script.js
+++ Develop/script.js
@@ -18,6 +18,13 @@ debugger;
   var length = passwordLength();
   console.log(length);
 
+  if (lowerCase()) {
+    var lowCase = "abcdefghijklmnopqrstuvwxyz";
+  }
+  else {
+    lowCase = "";
+  }
+
   if (capitalLetters()) {
     var capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
@@ -40,7 +47,7 @@ debugger;
   }
 
   var givenPassword = "";
-  var combinedChars = smallcaps + capital + special + num;
+  var combinedChars = lowCase + capital + special + num;
   var combinedCharsLength = combinedChars.length;
   for ( var i = 0; i < length; i++ ) {
     givenPassword += combinedChars.charAt(Math.floor(Math.random() * combinedCharsLength));
@@ -50,61 +57,49 @@ debugger;
   
 };
 
-// Small caps letters
-var smallcaps = "abcdefghijklmnopqrstuvwxyz";
-  console.log(smallcaps);
+
 
 
 // Get password length
-var passwordLength = function() {
-  var lengthInput = window.prompt("How long do you want your password to be? Please choose a number between 8 and 128.");
-
-  if (lengthInput < 8 || lengthInput > 128) {
+const passwordLength = function() {
+  var lengthInput;
+  while ((lengthInput = window.prompt("How long do you want your password to be? Please choose a number between 8 and 128.")), lengthInput < 8 || lengthInput > 128) 
     window.alert("Incorrect value. Please choose a number between 8 and 128.");
-    passwordLength();
-  }
-
   return lengthInput;
 };
 
-
-
+// Include small caps in password
+const lowerCase = function() {
+  var lowCase;
+  while ((lowCase = window.confirm("Would you like to include lower-case letters in your password?")), lowCase === null)
+    window.alert("No answer given. Please choose whether or not you'd like to include small-caps letters in your password.");
+  return lowCase;
+}
 
 // Add capital letters to password
-var capitalLetters = function() {
-  var capitalsInput = window.confirm("Would you like to include capital letters in your password?");
-
-  if (capitalsInput === null) {
-    window.alert("Please choose whether or not you'd like to include capital letters in your password.");
-    capitalLetters();
-  }
-  
+const capitalLetters = function() {
+  var capitalsInput;
+  while ((capitalsInput = window.confirm("Would you like to include upper-case letters in your password?")), capitalsInput === null)
+    window.alert("No answer given. Please choose whether or not you'd like to include capital letters in your password.");
   return capitalsInput;
 };
 
 // Add special characters to password
-var specialCharacters = function() {
-  var specialInput = window.confirm("Would you like to include special characters in your password?");
- 
-  if (specialInput === null) {
+const specialCharacters = function() {
+  var specialInput;
+  while ((specialInput = window.confirm("Would you like to include special characters in your password?")), specialInput === null)
     window.alert("Please choose whether or not you'd like to include special characters in your password.");
-    specialCharacters();
-  }
-  
   return specialInput;
 };
 
 // Add numbers to password
-var includeNumbers = function() {
-  var numberInput = window.confirm("Would you like to include numbers in your password?");
+const includeNumbers = function() {
+  var numberInput;
 
-  if (numberInput === null) {
+  while ((numberInput  = window.confirm("Would you like to include numbers in your password?")), numberInput === null)
     window.alert("Please choose whether or not you'd like to include numbers in your password.");
-    includeNumbers();
-  }
-  
   return numberInput;
-}
+};
 
 
 
