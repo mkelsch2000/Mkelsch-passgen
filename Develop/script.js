084fb69923fb66fb2d44f2f14b73e55420b396d5
Matthew Kelsch <95149182+mkelsch2000@users.noreply.github.com>
2022-01-01 19:35:07 -0700

    gave each group of symbols an array

--- Develop/script.js
+++ Develop/script.js
@@ -14,25 +14,70 @@ function writePassword() {
 }
 
 var generatePassword = function() {
-  //Choose Password Length
-  var length = passwordLength();
-  var capital = capitalLetters();
-  var special = specialCharacters();
-  var num = includeNumbers();
+
+  
+
+  if (capitalLetters()) {
+    var capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  else {
+    capital = "";
+  }
+  console.log(capital);
+
+  if (specialCharacters()) {
+    var special = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+  }
+  else {
+    special = "";
+  }
+  console.log(special);
+
+  if (includeNumbers()) {
+    var num = "0123456789";
+  }
+  else {
+    num = "";
+  }
+  console.log(num);
+
+  var combinedChars = smallcaps + capital + special + num;
+  console.log(combinedChars);
+
+  return combinedChars;
+  
   
 };
 
+
+// Password Length
+var length = passwordLength();
+  console.log(length);
+
+// Small caps letters
+var smallcaps = "abcdefghijklmnopqrstuvwxyz";
+  console.log(smallcaps);
+
+
 // Get password length
 var passwordLength = function() {
+
   var lengthInput = window.prompt("How long do you want your password to be? Please choose a number between 8 and 128.");
 
   if (lengthInput < 8 || lengthInput > 128) {
     window.alert("Incorrect value. Please choose a number between 8 and 128.");
     passwordLength();
   }
+  else if(lengthInput === null) {
+    window.alert("No number given. Please enter a number between 8 and 128.");
+    passwordLength();
+  }
 
   return lengthInput;
 };
+
+
+
 
 // Add capital letters to password
 var capitalLetters = function() {
@@ -42,7 +87,7 @@ var capitalLetters = function() {
     window.alert("Please choose whether or not you'd like to include capital letters in your password.");
     capitalLetters();
   }
-
+  
   return capitalsInput;
 };
 
@@ -54,7 +99,7 @@ var specialCharacters = function() {
     window.alert("Please choose whether or not you'd like to include special characters in your password.");
     specialCharacters();
   }
-
+  
   return specialInput;
 };
 
@@ -66,9 +111,11 @@ var includeNumbers = function() {
     window.alert("Please choose whether or not you'd like to include numbers in your password.");
     includeNumbers();
   }
-
+  
   return numberInput;
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
